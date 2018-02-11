40cf6c7b36d4f0c18aa1086e749064cc6f4c0776
Your Name <you@example.com>
2018-02-11 16:37:29 +0530

    First prediction added (Linear regression)

--- selection_validator.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import Form
-from wtforms import TextField,RadioField, SelectField,IntegerField,TextAreaField,SubmitField
-from wtforms import validators, ValidationError
-
-class ContactForm(Form):
-	
-	crime_type = SelectField('Crime Type', choices=[('against_women', 'Crime Against Women'), ('other', 'Other')])
-	visual_type = SelectField('Visualization Type', choices=[('barchart', 'Bar Chart'), ('other', 'Other')])
-	submit = SubmitField("Send")
